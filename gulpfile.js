c0947bea3ade6ce1a366fcedd2f9f8304d96c567
Rostislav <stillgrey@mail.ru>
2016-10-04 00:58:48 +0000

    Menu+main frame

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,23 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     refresh = require('gulp-refresh'),
-    cleanCSS = require('gulp-clean-css');
-
-// concatCss = require('gulp-concat-css'),
-// concat = require('gulp-concat'),
-// rename = require('gulp-rename'),
-// uncss = require('gulp-uncss'),
-// add = require('gulp-add'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat');
+    // concatCss = require('gulp-concat-css'),
+    // rename = require('gulp-rename');
+    // uncss = require('gulp-uncss'),
+    // add = require('gulp-add');
 
 
 gulp.task('scss', () => {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+            cascade: false
+        }))
     .pipe(gulp.dest('./dist/css'))
     .pipe(refresh());
 });
@@ -26,7 +30,7 @@ gulp.task('html', () => {
 });
 
 gulp.task('scripts', () => {
-  return gulp.src(['./js/bootstrap.min.js', './js/jquery.min.js'])
+  return gulp.src(['./js/jquery.min.js', './js/bootstrap.min.js'])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./dist/js'))
     .pipe(refresh());
